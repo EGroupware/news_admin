4ae21e1445b34c6e7f459e215f651989dbf6b4a9
skeeter <skeeter@alumni.egroupware.org>
2001-06-14 03:07:44 +0000

    Fix for bug 431196

---
+++ templates/default/news.tpl
@@ -0,0 +1,30 @@
+<!-- END news -->
+ <div align="right">
+  <table border="0" width="90%" cellspacing="0" cellpadding="0">
+  
+   {rows}
+  
+  </table>
+ </div>
+<!-- END news -->
+
+<!-- BEGIN row -->
+   <tr bgcolor="c7c3c7">
+    <td width="13" valign="top" valign="top">
+     <img src="{icon}" align="top">
+    </td>
+    <td align="left" width="99%">
+     <b>{subject}</b>&nbsp;
+    </td>
+    <td align="left" width="1%" bgcolor="FFFFFF">
+     &nbsp;
+    </td>
+   </tr>
+   <tr>
+    <td width="100%" colspan="3">
+     {submitedby}
+     <p>{content}</p>
+     <p>&nbsp;</p>
+    </td>
+   </tr>
+<!-- END row -->
--- view.php
+++ view.php
@@ -14,31 +14,32 @@
 
   /* $Id$ */
 
-	$phpgw_info['flags'] = array(
+	$phpgw_flags = Array(
 		'currentapp' => 'news_admin',
 		'enable_nextmatchs_class' => True
 	);
-	include('../header.inc.php');
 
-	// If you move your site templates, change your path to it here
-	//$tpl = new Template($phpgw_info["server"]["server_root"] . "/news_admin/website/templates");
-	$tpl = CreateObject('phpgwapi.Template',PHPGW_SERVER_ROOT . SEP . 'news_admin' . SEP . 'website' . SEP . 'templates');
+  $phpgw_info['flags'] = $phpgw_flags;
+  include('../header.inc.php');
 
-	$tpl->set_file(array(
-		'news' => 'news.tpl',
-		'row'  => 'news_row.tpl'
-	));
+  // If you move your site templates, change your path to it here
+  $tpl = CreateObject('phpgwapi.Template',PHPGW_APP_TPL);
 
-	$phpgw->db->query("SELECT * FROM webpage_news WHERE news_id='$news_id'",__LINE__,__FILE__);
-	$phpgw->db->next_record();
+  $tpl->set_file('news_form' => 'news.tpl');
 
-	$tpl->set_var('icon_dir','website/');
-	$tpl->set_var('subject',$phpgw->db->f('news_subject'));
-	$tpl->set_var('submitedby','Submitted by ' . $phpgw->db->f('submittedby') . ' on ' . date('m/d/Y - h:m:s a',$phpgw->db->f('news_date')));
-	$tpl->set_var('content',nl2br($phpgw->db->f('news_content')));
+  $tpl->set_block('news_form','news','news');
+  $tpl->set_block('news_form','row','row');
+  
+  $phpgw->db->query("select * from webpage_news where news_id='$news_id'",__LINE__,__FILE__);
+  $phpgw->db->next_record();
 
-	$tpl->parse('rows','row',True);
-	$tpl->pparse('out','news');
+  $tpl->set_var('icon',$phpgw->common->image('news_admin','news-corner.gif'));
+  $tpl->set_var('subject',$phpgw->db->f('news_subject'));
+  $tpl->set_var('submitedby','Submitted by ' . $phpgw->db->f('submittedby') . ' on ' . date('m/d/Y - h:m:s a',$phpgw->db->f('news_date')));
+  $tpl->set_var('content',nl2br($phpgw->db->f('news_content')));
 
-	$phpgw->common->phpgw_footer();
+  $tpl->parse('rows','row',True);
+  $tpl->pparse('out','news');
+
+  $phpgw->common->phpgw_footer();
 ?>
