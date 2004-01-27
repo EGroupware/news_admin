4fd280b09393f7b5f143934ea42d1bb957130a84
reinerj <reinerj@alumni.egroupware.org>
2004-01-27 23:27:03 +0000

    changes from phpgw to egw dev group

--- website/index.php
+++ website/index.php
@@ -1,8 +1,8 @@
 <body bgcolor="FFFFFF">
 <?php
   /**************************************************************************\
-  * phpGroupWare - Webpage news admin                                        *
-  * http://www.phpgroupware.org                                              *
+  * eGroupWare - Webpage news admin                                          *
+  * http://www.egroupware.org                                                *
   * --------------------------------------------                             *
   *  This program is free software; you can redistribute it and/or modify it *
   *  under the terms of the GNU General Public License as published by the   *
