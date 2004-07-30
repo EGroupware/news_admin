3df96feb1babe98ca278fa090c8ef7462585455c
shrykedude <shrykedude@alumni.egroupware.org>
2004-07-30 19:06:35 +0000

    Fixed app dependencies for api-1.0.1 version change

--- setup/setup.inc.php
+++ setup/setup.inc.php
@@ -31,6 +31,6 @@
 	/* Dependencies for this app to work */
 	$setup_info['news_admin']['depends'][] = array(
 		 'appname' => 'phpgwapi',
-		 'versions' => Array('0.9.14', '0.9.15','1.0.0')
+		 'versions' => Array('0.9.14', '0.9.15','1.0.0','1.0.1')
 	);
 
