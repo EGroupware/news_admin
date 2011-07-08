97ad1ab3b8f059aa09d0b63000a2e1d1def0f969
Nathan Gray <nathangray.bsc@gmail.com>
2011-07-08 19:45:15 +0000

    Changes to news categories: - Use owner of category for read permission, using standard category permissions.  Only writer uses extra ACL. - Add context menu for news categories.

--- inc/class.news_admin_ui.inc.php
+++ inc/class.news_admin_ui.inc.php
@@ -59,9 +59,10 @@ class news_admin_ui extends bonews
 			{
 				$content = array(
 					'cat_writable' => $this->user,
-					'cat_owner' => isset($GLOBALS['egw_info']['user']['apps']['admin']) ? -1 : $this->user,
+					'cat_owner' => isset($GLOBALS['egw_info']['user']['apps']['admin']) ? 0 : $this->user,
 				);
 			}
+			if($_GET['parent']) $content['cat_parent'] = (int)$_GET['parent'];
 		}
 		else
 		{
@@ -93,7 +94,6 @@ class news_admin_ui extends bonews
 				case 'save':
 					if ($content['import_url'] && $content['cat_writable'])
 					{
-						$content['cat_writable'] = '';
 						$msg = lang('Imported feeds can NOT be writable!').' ';
 					}
 					if (($content['cat_id'] = $this->save_cat($content)))
@@ -164,6 +164,12 @@ class news_admin_ui extends bonews
 			}
 			$readonlys['button[import]'] = $readonlys['button[delete]'] = $readonlys['button[save]'] = $readonlys['button[apply]'] = true;
 		}
+		if(!$GLOBALS['egw_info']['user']['apps']['admin'])
+		{
+			// Regular users can't give write access to others
+			$content['cat_owner'] = $content['cat_writable'] = array($GLOBALS['egw_info']['user']['account_id']);
+			$readonlys['cat_writable'] = true;
+		}
 		if (!$content['cat_id']) $readonlys['button[delete]'] = true;
 		if ($content['cat_id']) $readonlys['cat_owner'] = true;	// cat class can only set owner when creating new cats
 		if (!$content['import_url'] || !$content['cat_id']) $readonlys['button[import]'] = true;
@@ -190,21 +196,47 @@ class news_admin_ui extends bonews
 		if ($content['nm']['rows']['delete'])
 		{
 			list($id) = each($content['nm']['rows']['delete']);
-			if ($this->delete_cat($id))
-			{
-				$msg = lang('Category deleted.');
-			}
+			$content['nm']['action'] = 'delete';
+			$content['nm']['selected'] = array($id);
 		} else if ($content['nm']['rows']['update']) {
 			list($id) = each($content['nm']['rows']['update']);
-			require_once(EGW_INCLUDE_ROOT.'/news_admin/inc/class.news_admin_import.inc.php');
-			$import = new news_admin_import($this);
-			if ((list($imported,$newly,$deleted) = $import->import($id)) === false)
+			$content['nm']['action'] = 'update';
+			$content['nm']['selected'] = array($id);
+		}
+		if ($content['admin'] && $content['nm']['action'] == 'admin')
+		{
+			$content['nm']['action'] = $content['admin'];
+		}
+		if($content['nm']['action'])
+		{
+			if (!count($content['nm']['selected']) && !$content['nm']['select_all'])
 			{
-				$msg = lang('Error importing the feed!');
+				$msg = lang('You need to select some entries first');
 			}
 			else
 			{
-				$msg = lang('%1 news imported (%2 new, %3 deleted).',$imported,$newly,$deleted);
+				// Some processing to add values in for links and cats
+				$multi_action = $content['nm']['action'];
+				// Action has an additional action - add / delete, etc.  Buttons named <multi-action>_action[action_name]
+				if(in_array($multi_action, array('reader','writer')))
+				{
+					$content['nm']['action'] .= '_' . key($content[$multi_action . '_action']);
+
+					if(is_array($content[$multi_action]))
+					{
+						$content[$multi_action] = implode(',',$content[$multi_action]);
+					}
+					$content['nm']['action'] .= '_' . $content[$multi_action];
+				}
+				if ($this->action($content['nm']['action'],$content['nm']['selected'],$content['nm']['select_all'],
+					$success,$failed,$action_msg,'cats',$msg,$content['nm']['checkboxes']['no_notifications']))
+				{
+					$msg .= lang('%1 entries %2',$success,$action_msg);
+				}
+				elseif(is_null($msg))
+				{
+					$msg .= lang('%1 entries %2, %3 failed because of insufficent rights !!!',$success,$action_msg,$failed);
+				}
 			}
 		}
 		$content = array(
@@ -225,6 +257,9 @@ class news_admin_ui extends bonews
 				'col_filter'     =>	array(),	// IO array of column-name value pairs (optional for the filterheaders)
 				'no_filter'      => true,
 				'no_filter2'     => true,
+				'default_cols'   => '!legacy_actions',
+				'row_id'         => 'id',
+				'actions'        => $this->get_actions()
 			);
 		}
 		$this->tpl->read('news_admin.cats');
@@ -250,10 +285,204 @@ class news_admin_ui extends bonews
 		$readonlys = array();
 		foreach($rows as $k => $row)
 		{
-			$readonlys['edit['.$row['cat_id'].']'] = $readonlys['delete['.$row['cat_id'].']'] = !$this->admin_cat($row);
-			$readonlys['update['.$row['cat_id'].']'] = !(($this->admin_cat($row)) && ($row['import_url']));
+			$readonlys['edit['.$row['id'].']'] = $readonlys['delete['.$row['id'].']'] = !$this->admin_cat($row);
+			if(!$this->admin_cat($row)) $rows[$k]['class'] .= ' rowNoEdit rowNoDelete';
+			if($row['import_url']) 
+			{
+				$rows[$k]['class'] .= ' rowNoWriters'; // Imported news can't be edited
+			}
+			if($row['import_url'] && !$this->admin_cat($row) || !$row['import_url'])
+			{
+				$readonlys['update['.$row['id'].']'] = true;
+				$rows[$k]['class'] .= ' rowNoUpdate';
+			}
 		}
-		//_debug_array($rows);
 		return $total;
+	}
+
+	public function get_actions() 
+	{
+		$actions = array(
+			'open' => array(        // does edit if allowed, otherwise view
+				'caption' => 'Open',
+				'default' => true,
+				'allowOnMultiple' => false,
+				'url' => 'menuaction=news_admin.news_admin_ui.cat&cat_id=$id',
+				'popup' => '600x380',
+				'group' => $group=1,
+			),
+			'add' => array(
+				'caption' => 'Add',
+				'allowOnMultiple' => false,
+				'icon' => 'new',
+				'url' => 'menuaction=news_admin.news_admin_ui.cat',
+				'popup' => '600x380',
+				'group' => $group,
+			),
+			'sub' => array(
+				'caption' => 'Add sub',
+				'allowOnMultiple' => false,
+				'icon' => 'new',
+				'url' => 'menuaction=news_admin.news_admin_ui.cat&parent=$id',
+				'popup' => '600x380',
+				'group' => $group,
+				'disableClass' => 'rowNoSub',
+			),
+			'change' => array(
+				'caption' => 'Change',
+				'group' => $group,
+				'disableClass' => 'rowNoEdit',
+				'children' => array(
+				'reader' => array(
+					'caption' => 'Read permissions',
+					'icon' => 'users',
+					'nm_action' => 'open_popup',
+					'group' => $group,
+				),
+				'writer' => array(
+					'caption' => 'Write permissions',
+					'icon' => 'users',
+					'nm_action' => 'open_popup',
+					'group' => $group,
+					'disableClass' => 'rowNoWriters'
+				)),
+			),
+			'update' => array(
+				'caption' => 'Update RSS feed',
+				'allowOnMultiple' => true,
+				'disableClass' => 'rowNoUpdate',
+				'group' => $group,
+			),
+			'delete' => array(
+				'caption' => 'Delete',
+				'confirm' => 'Delete this category, and all news in it',
+                                'confirm_multiple' => 'Delete these categories, and all news in them',
+				'allowOnMultiple' => true,
+				'nm_action' => 'open_popup',
+				'group' => ++$group,
+				'disableClass' => 'rowNoDelete',
+			),
+		);
+
+		if(!$GLOBALS['egw_info']['user']['apps']['admin'])
+		{
+			unset($actions['change']['children']['writer']);
+		}
+
+		return $actions;
+	}
+
+	/**
+	 * apply an action to multiple entries
+	 *
+	 * @param string|int $action 'status_to',set status of entries
+	 * @param array $checked tracker id's to use if !$use_all
+	 * @param boolean $use_all if true use all entries of the current selection (in the session)
+	 * @param int &$success number of succeded actions
+	 * @param int &$failed number of failed actions (not enought permissions)
+	 * @param string &$action_msg translated verb for the actions, to be used in a message like %1 entries 'deleted'
+	 * @param string|array $session_name 'index' or 'email', or array with session-data depending if we are in the main list or the popup
+	 * @param string &$msg
+	 * @param boolean $no_notification
+	 * @return boolean true if all actions succeded, false otherwise
+	 */
+	function action($action,$checked,$use_all,&$success,&$failed,&$action_msg,$session_name,&$msg,$no_notification)
+	{
+		//echo '<p>'.__METHOD__."('$action',".array2string($checked).','.(int)$use_all.",...)</p>\n";
+		$success = $failed = 0;
+		if ($use_all)
+		{
+			// get the whole selection
+			@set_time_limit(0);                     // switch off the execution time limit, as it's for big selections to small
+			if(!is_array($session_name))
+			{
+				$old_query = $query = $GLOBALS['egw']->session->appsession($session_name,'news_admin');
+			}
+			else
+			{
+				$query = $session_name;
+			}
+			$query['num_rows'] = -1;        // all
+			$this->get_rows($query,$result,$readonlys);
+			$checked = array();
+			foreach($result as $key => $info)
+			{
+				if(is_numeric($key))
+				{
+					$checked[] = $info['id'];
+				}
+			}
+			// Reset query
+			if($old_query) $GLOBALS['egw']->session->appsession($session_name,'news_admin',$old_query);
+		}
+
+		list($action, $settings) = explode('_', $action, 2);
+
+		switch($action)
+		{
+			case 'delete':
+				$action_msg = lang('deleted');
+				foreach($checked as $id) {
+					if ($this->delete_cat($id))
+					{
+						$success++;
+					}
+					else
+					{
+						$failed++;
+					}
+				}
+				break;
+			case 'update':
+				require_once(EGW_INCLUDE_ROOT.'/news_admin/inc/class.news_admin_import.inc.php');
+				$import = new news_admin_import($this);
+				$imported = $newly = $deleted = 0;
+				$action_msg = lang('updated');
+				foreach($checked as $id) {
+					if ((list($_imported,$_newly,$_deleted) = $import->import($id)) === false)
+					{
+						$msg .= "\n".lang('Error importing the feed!')."\n";
+						$failed++;
+					}
+					else
+					{
+						$success++;
+						$imported += $_imported;
+						$newly += $_newly;
+						$deleted += $_deleted;
+					}
+				}
+				$msg .= lang('%1 news imported (%2 new, %3 deleted).',$imported,$newly,$deleted);
+				break;
+			case 'reader':
+			case 'writer':
+				$action_msg = lang('updated');
+				list($add_remove, $ids) = explode('_', $settings, 2);
+				$ids = explode(',',$ids);
+				$field = 'cat_'. ($action == 'reader' ? 'readable' : 'writable');
+
+				foreach($checked as $id)
+				{
+					if (!$data = $this->read_cat($id)) continue;
+					$data[$field] = $add_remove == 'add' ?
+						$ids == array(categories::GLOBAL_ACCOUNT) ? $ids : array_merge($data[$field],$ids) :
+						array_diff($data[$field],$ids);
+					$data[$field] = array_unique($data[$field]);
+					if ($this->save_cat($data))
+					{
+						$success++;
+						if($action == 'reader' && $add_remove == 'delete' && $kept = @array_intersect($ids, $data['cat_writable']))
+						{
+							$msg .= $data['cat_name'] . ': ' . lang('Kept %1 because of write permissions.  Remove them first.',count($kept)). "\n";
+						}
+					}
+					else
+					{
+						$failed++;
+					}
+				}
+				break;
+		}
+		return $failed == 0;
 	}
 }
