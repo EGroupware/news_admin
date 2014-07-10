2aaec0aba15f6e7718de887794a6476773090b12
Ralf Becker <ralfbecker@outdoor-training.de>
2014-07-10 14:23:11 +0000

    send Accept-Language HTTP header when querying news and change url of egroupware.org news feed to http://www.egroupware.org/start/rss.xml

--- inc/class.news_admin_import.inc.php
+++ inc/class.news_admin_import.inc.php
@@ -5,7 +5,7 @@
  * @link http://www.egroupware.org
  * @author Ralf Becker <RalfBecker-AT-outdoor-training.de>
  * @package news_admin
- * @copyright (c) 2007-11 by Ralf Becker <RalfBecker-AT-outdoor-training.de>
+ * @copyright (c) 2007-14 by Ralf Becker <RalfBecker-AT-outdoor-training.de>
  * @license http://opensource.org/licenses/gpl-license.php GPL - GNU General Public License
  * @version $Id$
  */
@@ -45,30 +45,50 @@ class news_admin_import
 	 * Read the feed of the given URL
 	 *
 	 * @param string $url
+	 * @param array $context=null
 	 * @return XML_Feed_Parser|boolean false on error
 	 */
-	function read($url)
+	function read($url, array $context=null)
 	{
+		$default_lang = $GLOBALS['egw']->preferences->default['common']['lang'];
+		$default_context = array(
+			'method'=>'GET',
+			'header' => 'Accept-Language: '.($default_lang ? $default_lang : 'en').';0.8,en;0.2',
+		);
 		$parts = parse_url($url);
 		if (!in_array($parts['scheme'],array('http','https','ftp'))) return false;	// security!
 
-		if (!($feed_xml = file_get_contents($url,false)) || !@include_once('XML/Feed/Parser.php'))
+		if (!($feed_xml = file_get_contents($url, false,
+			stream_context_create(array('http' => $context ? $context : $default_context)))) ||
+			!@include_once('XML/Feed/Parser.php'))
 		{
 			return false;
 		}
-
+		$matches = null;
 		// if the xml-file specifes an encoding, convert it to our own encoding
 		if (preg_match('/\<\?xml.*encoding="([^"]+)"/i',$feed_xml,$matches) && $matches[1])
 		{
-			$feed_xml = preg_replace('/(\<\?xml.*encoding=")([^"]+)"/i','$1'.$GLOBALS['egw']->translation->charset().'"',$feed_xml);
-			$feed_xml = $GLOBALS['egw']->translation->convert($feed_xml,$matches[1]);
+			$feed_xml = preg_replace('/(\<\?xml.*encoding=")([^"]+)"/i','$1'.translation::charset().'"',
+				translation::convert($feed_xml, $matches[1]));
 		}
 		// stop "unsupported encoding" warnings
 		error_reporting(($level = error_reporting()) & !E_WARNING);
 		try {
 		    $parser = new XML_Feed_Parser($feed_xml);
-		} catch (XML_Feed_Parser_Exception $e) {
-		    $parser = false;
+		}
+		catch (XML_Feed_Parser_Exception $e)
+		{
+			unset($e);	// not used
+			if (!$context)
+			{
+				// Try again with a user agent
+				$context = array('user_agent' => 'Mozilla/5.0')+$default_context;
+				$parser = $this->read($url, $context);
+			}
+			else
+			{
+				$parser = false;
+			}
 		}
 		error_reporting($level);
 
@@ -79,16 +99,33 @@ class news_admin_import
 	 * Import the feed of one category
 	 *
 	 * @param int $cat_id
-	 * @param string $url
 	 * @return array/boolean array(total imported,newly imported) or false on error
 	 */
-	function import($cat_id,$url)
+	function import($cat_id)
 	{
+		if (($cat = $this->bonews->read_cat($cat_id)) === false) return false;
+		if (! ($url = $cat['import_url'])) return false;
+		if (!isset($cat['keep_imported'])) $cat['keep_imported'] = -1; // keep all was the default.
+
 		$parser = $this->read($url);
 
 		if (!is_object($parser)) return false;
 
-		$imported = $newly = 0;
+		$imported = $newly = $deleted = 0;
+
+		$news_delete = array();
+		if ($cat['keep_imported'] >= 0)
+		{
+			$check = array('cat_id' => $cat_id);
+			$count = 0;
+			foreach($this->bonews->search($check,array('news_id'),'news_date DESC') as $news)
+			{
+				if (++$count > $cat['keep_imported']) {
+					$news_delete[$news['news_id']] = true;
+				}
+			}
+		}
+
 		foreach ($parser as $entry)
 		{
 			$content_is_html = $entry->content && strip_tags($entry->content) != $entry->content;
@@ -108,8 +145,13 @@ class news_admin_import
 			{
 				$check['news_content'] = $entry->link;
 			}
-			if (!$this->bonews->read($check))
+			if (($newsitem = $this->bonews->read($check)))
 			{
+				if (0 == $cat['keep_imported'])
+				{
+					unset($news_delete[$newsitem['news_id']]);
+				}
+			} else {
 				$this->bonews->init();
 				++$newly;
 			}
@@ -131,7 +173,14 @@ class news_admin_import
 			}
 			//var_dump($err); print "<li><a href=\"$entry->link\" target=\"_blank\">$entry->title</a></li>\n"; //_debug_array($this->bonews->data);
 		}
-		return array($imported,$newly);
+
+		$deleted = $news_delete ? $this->bonews->delete(array_keys($news_delete)) : 0;
+
+		/* Update the category timestamp on successful import */
+		$cat['import_timestamp'] = $this->bonews->now;
+		$this->bonews->save_cat($cat);
+
+		return array($imported,$newly,$deleted);
 	}
 
 	/**
@@ -140,6 +189,7 @@ class news_admin_import
 	 */
 	function async_import()
 	{
+		$cats = $nul = null;
 		if (!$this->bonews->get_cats(array(
 			'num_rows' => 999,
 			'start' => 0,
@@ -147,9 +197,9 @@ class news_admin_import
 
 		foreach($cats as $cat)
 		{
-			if ($cat['import_url'] && !((int)date('H') % $cat['import_frequency']))
+			if ($cat['import_url'] && $cat['import_frequency'] && !((int)date('H') % $cat['import_frequency']))
 			{
-				$this->import($cat['cat_id'],$cat['import_url']);
+				$this->import($cat['cat_id']);
 			}
 		}
 	}
