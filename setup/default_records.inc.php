2f2358adb0dd64c6a2b85c652eafd2afe0022fe7
Ralf Becker <ralfbecker@outdoor-training.de>
2014-07-10 13:49:22 +0000

    change url of egroupware.org news feed to http://www.egroupware.org/start/rss.xml

--- setup/default_records.inc.php
+++ setup/default_records.inc.php
@@ -6,7 +6,7 @@
  * @package news_admin
  * @subpackage setup
  * @author RalfBecker@outdoor-training.de
- * @copyright (c) 2010 by RalfBecker@outdoor-training.de
+ * @copyright (c) 2010-14 by RalfBecker@outdoor-training.de
  * @license http://opensource.org/licenses/gpl-license.php GPL - GNU General Public License
  * @version $Id$
  */
@@ -39,20 +39,20 @@ foreach(array(
 
 // Create egroupware.org news category importing news from egroupware.org readable by everyone
 $data = serialize(array(
-	'import_url' => 'http://www.egroupware.org/index.php?module=news_admin&cat_id=3,200',
+	'import_url' => 'http://www.egroupware.org/start/rss.xml',
 	'import_frequency' => 4,
 	'keep_imported' => 0,
 ));
 $oProc->query("INSERT INTO {$GLOBALS['egw_setup']->cats_table} (cat_parent,cat_owner,cat_access,cat_appname,cat_name,cat_description,last_mod,cat_data)
 	VALUES (0,$global_cat_owner,'public','news_admin','egroupware.org','News from egroupware.org',".time().",'$data')");
-$cat_id = $oProc->m_odb->get_last_insert_id($GLOBALS['egw_setup']->cats_table,'cat_id');
+$egw_cat_id = $oProc->m_odb->get_last_insert_id($GLOBALS['egw_setup']->cats_table,'cat_id');
 foreach(array(
 	$admingroup => 1,
 	$defaultgroup => 1,
 	$anonymous => 1,
 ) as $user => $rights)
 {
-	$GLOBALS['egw_setup']->add_acl('news_admin','L'.$cat_id,$user,$rights);
+	$GLOBALS['egw_setup']->add_acl('news_admin','L'.$egw_cat_id,$user,$rights);
 }
 // add import job
 $async = new asyncservice();
